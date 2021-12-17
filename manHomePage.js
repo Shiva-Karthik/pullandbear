ce2b9889edaaf75eae25ad923393a618210ebb2a
Mohit <singhalmohit011@gmail.com>
2021-12-17 13:25:16 +0530

    Home Page updated

---
+++ manHomePage.js
@@ -0,0 +1,6 @@
+document.querySelector("#cancel").addEventListener("click", function(){
+    document.querySelector("#searchBar").style.display = "none";
+})
+document.querySelector(".search").addEventListener("click", function(){
+    document.querySelector("#searchBar").style.display = "block";
+})
