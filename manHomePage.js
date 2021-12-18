746d2d6e0551cc6513d50c6a17b678259b945d1f
Shiva-Karthik <95852934+Shiva-Karthik@users.noreply.github.com>
2021-12-18 13:44:16 +0530

    Add files via upload
    updated Homepage

--- manHomePage.js
+++ manHomePage.js
@@ -24,7 +24,8 @@ document.querySelector(".signin").addEventListener("click", function(){
 document.querySelector(".createAccount-submit-btn").addEventListener("click", createAccount);
 var userData = JSON.parse(localStorage.getItem("userDatabase")) || [];
 function createAccount() {
-
+    document.querySelector(".hellobox").style.display = "block";
+    document.querySelector(".createaccountbox").style.display = "none";
 var email = document.querySelector("#email").value;
 var password = document.querySelector("#password").value;
 console.log(email,password)
@@ -36,5 +37,78 @@ var userCred = {
 };
 userData.push(userCred);
 localStorage.setItem("userDatabase", JSON.stringify(userData));
-window.location.href = "hello.html"
+    
 }
+
+// register area
+
+document.querySelector(".cancelbutton").addEventListener("click", function(){
+    document.querySelector(".createaccountbox").style.display = "none";
+})
+document.querySelector(".register-submit-btn").addEventListener("click", function(){
+    document.querySelector(".createaccountbox").style.display = "block";
+    document.querySelector(".loginbox").style.display = "none";
+})
+
+
+document.querySelector(".createAccount-submit-btn").addEventListener("click", createAccount);
+var userData = JSON.parse(localStorage.getItem("userDatabase")) || [];
+function createAccount() {
+var email = document.querySelector("#email").value;
+var password = document.querySelector("#password").value;
+console.log(email,password)
+
+var userCred = {
+  emailAdd: email,
+  pass: password,
+};
+userData.push(userCred);
+localStorage.setItem("userDatabase", JSON.stringify(userData));
+document.querySelector(".hellobox").style.display = "block";
+}
+
+// hello
+
+// function hello() {
+//     window.location.href = "hello.html"
+// }
+
+document.querySelector(".cancelbutton").addEventListener("click", function(){
+    document.querySelector(".hellobox").style.display = "none";
+})
+document.querySelector(".login-submit-btn").addEventListener("click", function(){
+    document.querySelector(".hellobox").style.display = "block";
+    document.querySelector(".loginbox").style.display = "none";
+})
+
+// logout
+
+document.querySelector("#logout").addEventListener("click", function(){
+    window.location.href = "manHomePage.html"
+})
+
+// login check details
+
+document.querySelector(".login-submit-btn").addEventListener("click", login);
+  var regdUsers = JSON.parse(localStorage.getItem("userDatabase"));
+  console.log(regdUsers)
+  function login() {
+    var email = document.querySelector("#email").value;
+    console.log(email)
+    var password = document.querySelector("#password").value;
+    var flag = true;
+    console.log(password)
+    for (var i = 0; i < regdUsers.length; i++) {
+      if (regdUsers[i].emailAdd === email && regdUsers[i].pass === password) {
+        console.log("registered user");
+        document.querySelector(".hellobox").style.display = "block";
+      } else {
+        flag = false;
+        break;
+      }
+    }
+    if(flag===false){
+        alert("Wrong Email address or Password");
+        window.location.href = "manHomePage.html"
+    }
+  }
